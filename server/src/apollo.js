90c03f12966e95b119746bbb0ebd143aa0a4ef59
fccJashCoda <personalrevolution2014@gmail.com>
2021-09-27 01:12:41 +0200

    added express middleware support to the apollo server

--- server/src/apollo.js
+++ server/src/apollo.js
@@ -1,26 +1,39 @@
 require('dotenv').config;
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const GiveawayApi = require('./dataSources/givaways');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => ({
-    giveawayAPI: new GiveawayApi(),
-  }),
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-server
-  .listen({
-    port: 4005,
-  })
-  .then(() => {
-    console.log(`
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+      giveawayAPI: new GiveawayApi(),
+    }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  app.use(cors());
+  app.use(morgan('tiny'));
+
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise((resolve) => httpServer.listen({ port: 4005 }, resolve));
+  console.log(`
     Server is running.
     Listening on port 4005
     Explore at https://studio.apollographql.com/sandbox
   `);
-  });
+}
+
+startApolloServer(typeDefs, resolvers);
